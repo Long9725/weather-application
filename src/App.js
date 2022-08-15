449ac24b531572927ead67de5feb39ae027aab4b
Long9725 <develop9725@gmail.com>
2022-08-15 22:54:34 +0900

    Add: connect openWeatherMap current weather api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 import Footer from './component/Footer.js';
 
-function moreInfo() {
-  return window.alert('For more info, call 1-800-CATSROK')
-}
-
 function App() {
+  const [search, setSearch] = useState()
+  const [allData, setAllData] = useState({
+    city: 'Seoul',
+    country: '',
+    temp: '',
+    humidity: '',
+    tempMin: '',
+    weatherId: '',
+    weatherIcon: ''
+  })
+
+  useEffect(() => {
+    fetchData(allData.city)
+  }, [])
+
+  const fetchData = async(city) => {
+    try {
+      const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${REACT_APP_API_KEY}&units=metric`)
+
+      await setAllData({
+        city: result.data.name,
+        country: result.data.sys.country,
+        temp: result.data.main.temp,
+        humidity: result.data.main.humidity,
+        tempMin: result.data.main.temp_min,
+        weatherId: result.data.weather[0].id,
+        weatherIcon: result.data.weather[0].icon
+      })
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  const handleSumbit = async (event) => {
+    console.log(search)
+    event.preventDefault()
+    await fetchData(search)
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+    console.log(search)
+  }
+
   return (
-    <div className='App'>
-      <h1 style={{fontSize:'52px'}}> Cats R us </h1>
-      <img className='img' alt='Very many cats' src='https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'/>
-      <p style={{fontSize:'20px'}}>Cats R Us are all about or fluffy feline friends - the cat ...</p>
-      <button onClick={moreInfo} className='button'>Click For More Info</button>
-      <br/>
-      <Footer></Footer>
-    </div>
+    <main>
+      <div className='App'>
+        <form onSubmit={handleSumbit}>
+          <input
+          value={search}
+          type='text'
+          name='city'
+          placeholder='City name'
+          onChange={handleChange}
+          />
+          <button htmlFor='city'>Search</button>
+        </form>
+        <section>
+          <h1>{allData.city}</h1>
+          <h2>{allData.country}</h2>  
+          <h3>Temperature</h3>
+          <p>{allData.temp}°C</p>
+          <h3>Minimum temperature</h3>
+          <p>{allData.tempMin}°C</p>
+          <h3>Humidity</h3>
+          <p>{allData.humidity}%</p>
+          <img src={`http://openweathermap.org/img/wn/${allData.weatherIcon}@4x.png`}/>
+        </section>
+      </div>
+    </main>
   );
 }
 
